e6c8b90dad459cd03bdb2450cd63fdb50a5d7e16
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-07-03 13:06:25 +0900

    product_class_id check

--- tests/class/fixtures/FixtureGenerator.php
+++ tests/class/fixtures/FixtureGenerator.php
@@ -662,9 +662,8 @@ class FixtureGenerator
                 $where);
         }
 
-        if (!is_array($product_class_ids)) {
-            var_dump($product_class_ids);
-        }
+        var_dump('createOrder');
+        var_dump($product_class_ids);
         $orderDetails = array_map(function ($product_class_id) use ($order_id) {
             return $this->createOrderDetailAsArray($product_class_id, $order_id);
         }, $product_class_ids);
