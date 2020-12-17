1b3f8c0d6c69b9afabb35fe0baaf591e25a91080
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-12-17 17:19:36 +0900

    ADMIN_DIR 以外での認証を確認

--- ctests/acceptance/admin/AdminHomeCept.php
+++ ctests/acceptance/admin/AdminHomeCept.php
@@ -1,8 +1,20 @@
 <?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('管理画面に正常にログインできるかを確認する');
-$I->amOnPage('/'.getenv('ADMIN_DIR'));
 
+if (getenv('ADMIN_DIR') != 'admin/') {
+    $html_dir = __DIR__.'/../../../html/';
+    if (!file_exists($html_dir.'admin')) {
+        mkdir($html_dir.'admin');
+        copy($html_dir.getenv('ADMIN_DIR').'home.php', $html_dir.getenv('ADMIN_DIR').'home.php');
+    }
+
+    $I->expect('/admin/home.php での認証を確認します');
+    $I->amOnPage('/admin/home.php');
+    $I->see('ログイン認証の有効期限切れの可能性があります。');
+}
+
+$I->amOnPage('/'.getenv('ADMIN_DIR'));
 $I->fillField('input[name=login_id]', 'admin');
 $I->fillField('input[name=password]', 'password');
 $I->click(['css' => '.btn-tool-format']);
